98b51915918105432318b79032b798d3fc8c3f39
Antonio <antonioexposito6@hotmail.com>
2018-11-27 16:30:11 +0100

    First commit

---
+++ src/main/java/com/udl/nemeum/NemeumApplication.java
@@ -0,0 +1,12 @@
+package com.udl.nemeum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NemeumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NemeumApplication.class, args);
+	}
+}
---
+++ src/test/java/com/udl/nemeum/NemeumApplicationTests.java
@@ -0,0 +1,16 @@
+package com.udl.nemeum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NemeumApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
